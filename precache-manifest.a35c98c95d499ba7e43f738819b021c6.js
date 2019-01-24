5a02e07fc9137491c2263bdeaee1081f1249e1f7
Amit Tiwary <amittiwary710@gmail.com>
2019-01-25 02:33:33 +0530

    Updates

---
+++ precache-manifest.a35c98c95d499ba7e43f738819b021c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c0436d55f12af99760ca65640d19d19",
+    "url": "/coin-fundu/static/media/logo.5c0436d5.png"
+  },
+  {
+    "revision": "d22c9acaede58f87216b",
+    "url": "/coin-fundu/static/js/runtime~main.d22c9aca.js"
+  },
+  {
+    "revision": "f2cc4ef395ccf092c69f",
+    "url": "/coin-fundu/static/js/main.f2cc4ef3.chunk.js"
+  },
+  {
+    "revision": "3da4543213d710c1bc71",
+    "url": "/coin-fundu/static/js/1.3da45432.chunk.js"
+  },
+  {
+    "revision": "f2cc4ef395ccf092c69f",
+    "url": "/coin-fundu/static/css/main.3e9a58e8.chunk.css"
+  },
+  {
+    "revision": "5ac9fcd8a7f0ca788e3fb35957fc59e8",
+    "url": "/coin-fundu/index.html"
+  }
+];
